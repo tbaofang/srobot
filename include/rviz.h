b6a6dcd4226b7bb3933fefe03906964b86b7af3c
tbaofang <972409256@qq.com>
2018-05-26 20:31:52 +0800

    samples + rviz

--- include/rviz.h
+++ include/rviz.h
@@ -1,20 +1,10 @@
 #ifndef RVIZ_H
 #define RVIZ_H
 
-#include "msg.h"
-
 #include <geometry_msgs/PoseStamped.h>
 #include <geometry_msgs/Quaternion.h>
 #include <nav_msgs/Path.h>
 #include <ros/ros.h>
 #include <tf/tf.h>
-
-class Rviz
-{
-    public:
-        ros::Publisher path_pub;
-        Rviz(int argc, char** argv);
-        void Path(const Pose &pose, double th, int argc, char** argv);
-};
 
 #endif
--- src/samples.cpp
+++ src/samples.cpp
@@ -7,7 +7,7 @@
 void Samples::DrawCircle(int argc, char** argv)
 {
     Pose pose;
-    Rviz rv(argc, argv);
+//    Rviz rv(argc, argv);
 
     double x = 0.0;
     double y = 0.0;
@@ -20,8 +20,15 @@ void Samples::DrawCircle(int argc, char** argv)
     gettimeofday(&last_time, NULL);
     gettimeofday(&current_time, NULL);
 
+
+    ros::init(argc, argv, "showpath");
+    ros::NodeHandle n;
+    ros::Publisher path_pub = n.advertise<nav_msgs::Path>("trajectory", 1, true);
+    nav_msgs::Path path;
+
     Commom com;
-    while(true){
+    // while(true){
+    while(ros::ok()){
         gettimeofday(&current_time, NULL);
         double dt = current_time.tv_sec-last_time.tv_sec + (double)(current_time.tv_usec - last_time.tv_usec)/1000000;
         double delta_x = (vx*cos(th) - vy*sin(th))*dt;
@@ -35,8 +42,29 @@ void Samples::DrawCircle(int argc, char** argv)
         pose.position.x = x;
         pose.position.y = y;
         cout << dt << " " << pose.position.x << " " << pose.position.y << endl;
-        rv.Path(pose, th, argc, argv);
         gettimeofday(&last_time, NULL);
         com.Sleep(1000);
+
+        // Path(pose, th, argc, argv);
+
+
+        path.header.stamp = ros::Time::now();
+        path.header.frame_id = "odom";
+        geometry_msgs::PoseStamped this_pose_stamped;
+        this_pose_stamped.pose.position.x = pose.position.x;
+        this_pose_stamped.pose.position.y = pose.position.y;
+        geometry_msgs::Quaternion goal_quat = tf::createQuaternionMsgFromYaw(th);
+        this_pose_stamped.pose.orientation.x = goal_quat.x;
+        this_pose_stamped.pose.orientation.y = goal_quat.y;
+        this_pose_stamped.pose.orientation.z = goal_quat.z;
+        this_pose_stamped.pose.orientation.w = goal_quat.w;
+        this_pose_stamped.header.stamp = ros::Time::now();
+        this_pose_stamped.header.frame_id = "odom";
+        path.poses.push_back(this_pose_stamped);
+        path_pub.publish(path);
+        ros::spinOnce();
+        cout << "path" << " " << pose.position.y << endl;
+
+
     }
 }
